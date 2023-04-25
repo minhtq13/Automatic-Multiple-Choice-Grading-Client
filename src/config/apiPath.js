f2eb8abad977e18c9ba5691941f3473cf6dfe812
minhtq13 <taminh596@gmail.com>
2023-04-25 08:50:05 +0700

    base structure #2

--- src/config/apiPath.js
+++ src/config/apiPath.js
@@ -1,9 +1,4 @@
 // export const BASE_URL = "https://61fe8c59a58a4e00173c98cc.mockapi.io";
 export const BASE_URL = "http://localhost:8080";
 
-export const apiPath = {
-  // Account
-  login: BASE_URL + "/auth/login", // url api login
-  getUser: BASE_URL + "/users/findByUsernameAndPassword",
-  register: BASE_URL + "/api/user/signup", // url api register
-};
+export const apiPath = {};
