097842d8b1012464d16546cf4c955ef87ea9b365
minhtq13 <taminh596@gmail.com>
2023-04-26 10:02:03 +0700

    config structure

--- src/layout/DefaultLayout/index.jsx
+++ src/layout/DefaultLayout/index.jsx
@@ -1,13 +1,13 @@
 import "./DefaultLayout.scss";
-import Nav from "./Nav";
-import Footer from "./footer/footer";
 
 function DefaultLayout({ children }) {
   return (
     <div className="wrapper-default-layout">
-      {/* <Nav /> */}
-      <div className="container-content">{children}</div>
-      {/* <Footer /> */}
+      {/* <Header/> */}
+      <div className="">
+        {/* <Sidebar/> */}
+        <div className="container-content">{children}</div>
+      </div>
     </div>
   );
 }
